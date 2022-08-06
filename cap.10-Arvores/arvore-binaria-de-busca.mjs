fa0e9f57db7cdc355f6911a184a0b41942425784
Dsaniel_Santos <ds.99102811@gmail.com>
2022-08-06 17:43:55 -0300

    percorrendo uma arvore

--- cap.10-Arvores/arvore-binaria-de-busca.mjs
+++ cap.10-Arvores/arvore-binaria-de-busca.mjs
@@ -7,23 +7,23 @@ export default class BinarySearchTree {
         this.root = null;
     }
 
-    insert(key){
-        if(this.root = null){
+    insert(key) {
+        if (this.root == null) {
             this.root = new Node(key);
-        }else {
+        } else {
             this.insertNode(this.root, key)
         }
     }
 
-    insertNode(node, key){
-        if( this.compareFn(key, node.key) === Compare.LESS_THAN){
-            if(node.left == null){
+    insertNode(node, key) {
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            if (node.left == null) {
                 node.left = new Node(key);
-            }else {
+            } else {
                 this.insertNode(node.left, key)
             }
         } else {
-            if(node.right == null){
+            if (node.right == null) {
                 node.right = new Node(key);
             } else {
                 this.insertNode(node.right, key);
@@ -31,4 +31,64 @@ export default class BinarySearchTree {
         }
     }
 
+    inOrderTraverse(callback) {
+        this.inOrderTraverseNode(this.root, callback);
+    }
+
+    inOrderTraverseNode(node, callback) {
+        if (node != null) {
+            this.inOrderTraverseNode(node.left, callback);
+            callback(node.key)
+            this.inOrderTraverseNode(node.right, callback)
+        }
+    }
+
+    preOrderTraverse(callback){
+        this.preOrderTraverseNode(this.root, callback)
+    }
+    preOrderTraverseNode(node, callback){
+        if(node != null){
+            callback(node.key)
+            this.preOrderTraverseNode(node.left, callback);
+            this.preOrderTraverseNode(node.right, callback)
+
+        }
+    }
+
+
+
+
 }
+const tree = new BinarySearchTree();
+tree.insert(11)
+tree.insert(7)
+tree.insert(15)
+tree.insert(5)
+tree.insert(3)
+tree.insert(9)
+tree.insert(8)
+tree.insert(10)
+tree.insert(13)
+tree.insert(12)
+tree.insert(14)
+tree.insert(20)
+tree.insert(18)
+tree.insert(25)
+tree.insert(6)
+
+const printNode = (value) => console.log(value);
+tree.preOrderTraverse(printNode)
+//console.log(tree)
+
+
+
+
+
+
+
+
+
+
+
+
+
